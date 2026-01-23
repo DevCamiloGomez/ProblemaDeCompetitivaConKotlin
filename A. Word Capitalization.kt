4888c3e36b13941fed511b2ca7f15e53ec8c52d6
DevCamiloGomez <camiloalonsogoca@gmail.com>
2026-01-22 23:50:34 -0500

    A. Word Capitalization solved

---
+++ A. Word Capitalization.kt
@@ -0,0 +1,22 @@
+fun main(){
+
+    /*
+    Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
+
+Note, that during capitalization all the letters except the first one remains unchanged.
+
+Input
+A single line contains a non-empty word. This word consists of lowercase and uppercase English letters. The length of the word will not exceed 103.
+
+Output
+Output the given word after capitalization.
+
+Examples
+    */
+    val n = readLine()!!
+
+    val capitalizedStr = n.replaceRange(0, 1, n[0].uppercase())
+
+    println(capitalizedStr) // Output: Example
+
+}
